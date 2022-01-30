4fbb068beaeb640c1c5fbb098f9cc9ed6c7d6f16
Evg_Shcherbakov <shcherbakoveugeniy@gmail.com>
2022-01-30 18:49:00 +0200

    add auto link on loginpage

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,6 +3,11 @@ from .locators import LoginPageLocators, MainPageLocators
 
 
 class LoginPage(BasePage):
+    
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
 
     def should_be_login_url(self):
         assert "login" in self.browser.current_url, "Substring 'login' in link not found"
