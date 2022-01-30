6a18b7399425978b7da3626b3ba17da7647b0cd5
Evg_Shcherbakov <shcherbakoveugeniy@gmail.com>
2022-01-30 21:06:32 +0200

    change product_page structure

--- pages/product_page.py
+++ pages/product_page.py
@@ -19,18 +19,34 @@ class ProductPage(BasePage):
     def solve_allert_quiz(self):
         assert self.solve_quiz_and_get_code(), "Allert code not found"
 
-    def should_equal_product_name_in_cart(self):
+    def check_product_name_on_page_and_product_name_in_cart(self):
+        self.should_be_product_name()
+        self.should_be_product_name_in_cart()
+        self.should_equal_product_names()
+
+    def should_be_product_name(self):
         assert self.is_element_present(*ProductPageLocators.PRODUCT_NAME), "Product name not found"
+
+    def should_be_product_name_in_cart (self):
         assert self.is_element_present(*ProductPageLocators.PRODUCT_IN_CART_NAME), "Product name added to cart not found"
+
+    def should_equal_product_names(self):
         Product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
         Product_name_in_cart = self.browser.find_element(*ProductPageLocators.PRODUCT_IN_CART_NAME).text
         assert Product_name == Product_name_in_cart, "Product name not equal"
-        print(Product_name, Product_name_in_cart)  
 
-    def should_equal_product_price_in_cart(self):
+    def check_product_price_on_page_and_product_price_in_cart(self):
+        self.should_be_product_price()
+        self.should_be_product_price_in_cart()
+        self.should_equal_product_price()    
+
+    def should_be_product_price(self):
         assert self.is_element_present(*ProductPageLocators.PRODUCT_PRICE), "Product price not found"
+
+    def should_be_product_price_in_cart(self):
         assert self.is_element_present(*ProductPageLocators.CART_PRICE), "Cart price not found"
+
+    def should_equal_product_price(self):
         Product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
         Cart_price = self.browser.find_element(*ProductPageLocators.CART_PRICE).text
-        assert Product_price == Cart_price, "Product name not equal"
-        print(Product_price, Cart_price)  
+        assert Product_price == Cart_price, "Product price not equal" 
--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,9 @@
 from .pages.product_page import ProductPage
 
 def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
     page.add_item_to_cart()
-    page.should_equal_product_name_in_cart()
-    page.should_equal_product_price_in_cart()
+    page.check_product_name_on_page_and_product_name_in_cart()
+    page.check_product_price_on_page_and_product_price_in_cart()
